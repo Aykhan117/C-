33956fe0d67cd0048d158cd43f4cf8a6d82fe2de
Aykhan117 <karimovayxan51@gmail.com>
2026-07-03 11:29:43 +0400

    Create while.cpp

---
+++ while.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num = 1;
+    while (num < 1){
+        cout<<num<<" ";
+        num++;
+        
+    }
+    
+    num = 1;
+    do {
+        cout<<num<<" ";
+        num++;
+        
+    } while (num < 1);
+    
+    return 0;
+}
